1b793d9040ba54ccaddaf72bdadb87421302c311
Manooj7 <manojsambari2003@gmail.com>
2023-02-14 22:57:11 +0530

    new additions

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
+	res.sendFile(__dirname+"/index.html");
+});
+app.get("/resume", function(req, res){
 	res.sendFile(__dirname+"/Resume.html");
+});
+app.get("/card", function(req, res){
+	res.sendFile(__dirname+"/card.html");
 });
 
 app.listen(port, function(){
