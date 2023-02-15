eb98ac663432bf9ae8ee34bf000ee3b76ead6664
Manooj7 <manojsambari2003@gmail.com>
2023-02-15 21:56:43 +0530

    new edits

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const mongoose=require("mongoose");
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
@@ -15,8 +18,17 @@ app.get("/resume", function(req, res){
 app.get("/card", function(req, res){
 	res.sendFile(__dirname+"/card.html");
 });
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
+	if(err){
+		console.error(err);
+	}
+	else{
+		console.log("DB Connected");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+	}
 });
+
