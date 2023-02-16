a69c8b5112bfa53fa11e8f2039241714ddd7b692
Manooj7 <manojsambari2003@gmail.com>
2023-02-16 22:09:48 +0530

    new edits

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose=require("mongoose");
 
 const userSchema=new mongoose.Schema({
     userName : { type:String,required: true,unique: true},
+   // Dateofbirth:{type:String,require:true},
     yearOfGraduation: {type : Number,min: 2000,max:9000},
     createdAt :{type : Date,default: Date.now},
     isDeleted: {type:Boolean,default:false}
