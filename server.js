c181c13a0b06a0a799865c134fb466b415f01f19
Manooj7 <manojsambari2003@gmail.com>
2023-02-20 23:33:59 +0530

    edits!!!

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const userLib = require("./backend/lib/userLib");
+const todoLib = require("./backend/lib/todoLib");
 const mongoose=require("mongoose");
 
 const express = require('express');
@@ -9,10 +10,52 @@ const options = {
     extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
     index: ['index.html']
 }
-app.use(express.static("public", options));
+app.use(express.json());//whatever front end sends to us it will put it express.json!
+app.use(express.static("public",options));
 app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html");
 });
+app.get("/api/todos", function(req, res){
+	todoLib.getAllTodos(function(err,todos){
+		if(err){
+			res.json({status:"error",message:err,data:null});
+		}
+		else{
+			res.json({status:"success",data:todos});
+		}
+	});
+});
+
+app.post("/api/todos",function(req,res){
+	const todo=req.body;
+	todoLib.createTodo(todo,function(err,dbtodo){
+		if(err){
+			res.json({status:"error",message:err,data:null});
+		}
+		else{
+			res.json({status:"success",data:dbtodo});
+		}
+	})
+})
+app.put("/api/todos/:todoid",function(req,res){
+	const todo=req.body;
+	const todoid=req.params.todoid;
+
+	todoLib.updateTodoById(todoid,todo,function(err,dbtodo){
+		if(err){
+			res.json({status:"error",message:err,data:null});
+		}
+		else{
+			res.json({status:"success",data:dbtodo});
+		}
+	});
+})
+app.delete("/api/todos/id",function(req,res){
+	const todoid=req.params.todoid;
+	todoLib.deleteTodoById(todoid,function(err,dbtodo){
+
+	})
+})   
 app.get("/resume", function(req, res){
 	res.sendFile(__dirname+"/Resume.html");
 });
@@ -29,14 +72,14 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 	}
 	else{
 		console.log("DB Connected");
-		userLib.getAllUsers(function(err,result){
-			if(err){
-				console.error(err);
-			}
-			else{
-				console.log(result);
-			}
-		})
+		// userLib.getAllUsers(function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// })
 		//TODO: Donot Create a user if atleast 1 user exist in the table
 		// userLib.createFirstUser(function(err,result){
 		// 	if(err){
@@ -54,14 +97,14 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 		//  		console.log(result);
 		// 	}
 		// });
-		userLib.updateUser("Manoj Sambari","2020",function(err,result){
-			if(err){
-				console.error(err);
-			}
-			else{
-				console.log(result);
-			}
-		});
+		// userLib.updateUser("Manoj Sambari","2020",function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
 		// userLib.deleteUser("Manoj Sambari",function(err,result){
 		// 	if(err){
 		// 		console.error(err);
